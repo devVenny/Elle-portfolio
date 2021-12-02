96a480386032f29c7e7d6814d8b4625f03bcb747
vennylee <hyungsup42134@gmail.com>
2021-12-02 18:09:33 +0900

    feat(footer): Create top button when scroll go through home

--- components/header.js
+++ components/header.js
@@ -1,20 +1,33 @@
 "use strict";
 
 //// 스크롤 시 header 높이 줄이기
-// scroll이 32px 만큼 내려왔을 때
-// header 의 높이가 줄어든다. 얼만큼? 28px 정도?
-// with transition
+// - scroll이 32px 만큼 내려왔을 때
+// - header 의 높이가 줄어든다. 얼만큼? 28px 정도?
+// - with transition
 
-const header = document.getElementById("header");
+//// 스크롤이 home 화면을 지나면 footer에 top button 생성(누르면 화면이 한 페이지만큼 올라간다)
+// - Scroll 위치(a), Viewport 높이(b)
+// - a >= b 일 때 top button 생성
+
+const topButton = document.querySelector(".top-button");
 
 window.addEventListener("scroll", handleHeaderH);
+
 function handleHeaderH() {
+  const body = document.body;
+  const header = document.getElementById("header");
   const scrollLocation = window.scrollY;
-  if (scrollLocation > 32) {
+  const windowHeight = window.innerHeight;
+  const bodyHeight = body.scrollHeight;
+  if (scrollLocation > 100) {
     header.classList.add("shrink");
   } else {
     header.classList.remove("shrink");
   }
-}
 
-// 스크롤이 최하단으로 이동 시 top button 생성
+  if (scrollLocation >= windowHeight) {
+    topButton.classList.remove("hidden");
+  } else {
+    topButton.classList.add("hidden");
+  }
+}
